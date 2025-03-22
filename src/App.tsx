6267eaabe5f896f903fb631fd9fca244ac2deaae
carlos-ss <santillansoriano00@gmail.com>
2025-03-21 19:10:47 -0600

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 // import { useState } from 'react'
-
-
+import "./globals.css";
 
 function App() {
-
   return (
     <>
-      new app
+      <div className="bg-green-500 w-full h-full ">hi</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
