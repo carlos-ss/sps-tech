53b5453a79c207aec21ad9d1bb03b6c0a43bc80f
carlos-ss <santillansoriano00@gmail.com>
2025-03-21 22:19:08 -0600

    react query set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 // import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./globals.css";
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <div className="bg-green-500 w-full h-full ">hi</div>
+      <QueryClientProvider client={queryClient}>
+        <div className="bg-green-500 w-full h-full ">hi</div>
+      </QueryClientProvider>
     </>
   );
 }
