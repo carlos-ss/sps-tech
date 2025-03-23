d5a16b02172d01129b11429599a80d26f6ac9eb8
carlos-ss <santillansoriano00@gmail.com>
2025-03-22 18:51:26 -0600

    initial test set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    exclude: [...configDefaults.exclude, "dist/**"],
   },
 });
