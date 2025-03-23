4f0661edecb382ad14a97ac161ff15627feb483d
carlos-ss <santillansoriano00@gmail.com>
2025-03-23 16:33:17 -0600

    fix: center layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Flowbite } from "flowbite-react";
 
 const Layout = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
+    <Flowbite>
+      <div className="min-h-screen flex flex-col justify-between">
+        <Header />
+        <main className="flex-grow flex items-center justify-center">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </Flowbite>
   );
 };
 
