844b2f28c13d2d73b6389afe4415d33debe107c4
carlos-ss <santillansoriano00@gmail.com>
2025-03-23 10:29:47 -0600

    fix: api interceptor

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -12,12 +12,15 @@ export const api = axios.create({
 
 // FIXME: identify how to handle the error messages
 
-api.interceptors.request.use(
+api.interceptors.response.use(
   (response) => response,
   (error) => {
+    console.log("this");
     if (error.response.status === 400) {
       error.message = "Error making the request try again later";
       // console.log;
+    } else if (error.response.status === 401) {
+      error.message = "Unauthorized, incorrect credentials";
     }
     return Promise.reject(error);
   }
