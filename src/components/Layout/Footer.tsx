cbde58eb2900cad00052fd28d20eebcebf674bbd
carlos-ss <santillansoriano00@gmail.com>
2025-03-23 23:47:26 -0600

    upd: footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,15 +1,26 @@
 import { userStore } from "@/store";
 
 const Footer = () => {
-  const { loggedIn } = userStore((state) => state);
-
   return (
-    <footer>
-      {loggedIn ? (
-        <h1>Shop links</h1>
-      ) : (
-        <h1>Login link or create account link</h1>
-      )}
+    <footer className="flex justify-between items-center">
+      <div aria-label="footer left section">
+        <img
+          src="https://i0.wp.com/spsolutions.com.mx/wp-content/uploads/2022/11/Logo-300x150-Px.png?fit=300%2C150&ssl=1"
+          alt="sps"
+          width={100}
+        />
+      </div>
+
+      <div aria-label="footer center section">
+        <p className="text-center inline-flex items-center">
+          <a href="https://github.com/carlos-ss">Carlos-SS © 2025</a>
+          <img src="/clss-rls.png" alt="self-brand" width={100} />
+        </p>
+      </div>
+
+      <div aria-label="footer right section">
+        <img src="/logo.png" alt="logo" width={100} />
+      </div>
     </footer>
   );
 };
--- src/components/Layout/__tests__/Footer.spec.tsx
+++ src/components/Layout/__tests__/Footer.spec.tsx
@@ -1,34 +1,39 @@
 import { render, screen } from "@testing-library/react";
 import Footer from "../Footer";
-import { vi } from "vitest";
-import { userStore } from "@/store";
-
-vi.mock("@/store", () => ({
-  userStore: vi.fn(),
-}));
-const mockUserStore = (loggedIn: boolean) => {
-  (userStore as vi.Mock).mockImplementation(() => ({
-    loggedIn,
-    setLoggedIn: vi.fn(),
-  }));
-};
 
 describe("Footer", () => {
-  it("renders 'Login link or create account link' when the user is not logged in", () => {
-    mockUserStore(false);
-
+  it("renders the footer with the left section image", () => {
     render(<Footer />);
 
-    expect(
-      screen.getByText("Login link or create account link")
-    ).toBeInTheDocument();
+    const leftImage = screen.getByAltText("sps");
+    expect(leftImage).toBeInTheDocument();
+    expect(leftImage).toHaveAttribute(
+      "src",
+      "https://i0.wp.com/spsolutions.com.mx/wp-content/uploads/2022/11/Logo-300x150-Px.png?fit=300%2C150&ssl=1"
+    );
   });
 
-  it("renders 'Shop links' when the user is logged in", () => {
-    mockUserStore(true);
-
+  it("renders the footer with the center section text and image", () => {
     render(<Footer />);
 
-    expect(screen.getByText("Shop links")).toBeInTheDocument();
+    const centerText = screen.getByText(/Carlos-SS © 2025/i);
+    expect(centerText).toBeInTheDocument();
+    expect(centerText.closest("a")).toHaveAttribute(
+      "href",
+      "https://github.com/carlos-ss"
+    );
+
+    const centerImage = screen.getByAltText("self-brand");
+    expect(centerImage).toBeInTheDocument();
+    expect(centerImage).toHaveAttribute("src", "/clss-rls.png");
+  });
+
+  it("renders the footer with the right section image", () => {
+    render(<Footer />);
+
+    const rightImage = screen.getByAltText("logo");
+
+    expect(rightImage).toBeInTheDocument();
+    expect(rightImage).toHaveAttribute("src", "/logo.png");
   });
 });
