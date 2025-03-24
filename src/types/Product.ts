37f5e7f87f74fb62bdf9e20b4c3a783a1e1aee9a
carlos-ss <santillansoriano00@gmail.com>
2025-03-24 10:49:05 -0600

    upd: product types

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -5,4 +5,26 @@ export interface IProduct {
   description: string;
   category: string;
   image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+export interface IProductFilter {
+  category: string;
+  priceFrom: string;
+  priceTo: string;
+  rating: number;
+  sortOrder: string;
+}
+export interface IProductListPros {
+  filters: IProductFilter;
+  itemsPerPage: number;
+}
+
+export interface IProductFilterDrawerProps {
+  open: boolean;
+  onClose: () => void;
+  onFilterChange: (filters: IProductFilter) => void;
 }
